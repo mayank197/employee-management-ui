11897ed22423d20cf95204ede193c108de03f3f1
Mayank Sharma <mayank.1996.2014@gmail.com>
2020-06-27 19:50:17 +0530

    Project Outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { DepartmentDashboardComponent } from './department-dashboard/department-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDashboardComponent,
+    DepartmentDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/department-dashboard/department-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-dashboard',
+  templateUrl: './department-dashboard.component.html',
+  styleUrls: ['./department-dashboard.component.css']
+})
+export class DepartmentDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
